8f40e89a8813c89da5373f1959e53bfd47b5dffa
Bruno de Moraes <bdmrusso@gmail.com>
2019-04-07 17:57:58 -0300

    Feature: App done

---
+++ iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iamrich
+//
+//  Created by Bruno Russo on 07/04/19.
+//  Copyright © 2019 Bruno Russo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
